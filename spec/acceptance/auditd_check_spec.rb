1764c9706352a92a9c8d00e1d3f343fcec64c338
Luther Barnum <luther.barnum@gmail.com>
2015-04-08 23:34:22 -0400

    properly formatted and encapsulated describe statements

--- spec/acceptance/auditd_check_spec.rb
+++ spec/acceptance/auditd_check_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper_acceptance'
 
 describe 'auditd class' do
-  describe 'apply audit module' do
-    it 'should work with no errors' do
+  describe 'apply auditd module' do
+    it 'should apply with no errors' do
       pp = <<-EOS
       class { 'auditd': }
       EOS
@@ -11,34 +11,34 @@ describe 'auditd class' do
       apply_manifest(pp, :catch_changes => true)
     end
   end
-end
-
-describe 'auditd::install class' do
-  describe package('audit') do
-    it { should be_installed }
+  
+  describe 'auditd::install class' do
+    describe package('audit') do
+      it { should be_installed }
+    end
   end
-end
-
-describe 'auditd::config class' do
-  describe file('/etc/audit/auditd.conf') do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    it { should be_mode '640' }
+  
+  describe 'auditd::config class' do
+    describe file('/etc/audit/auditd.conf') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode '640' }
+    end
+    describe file('/etc/audit/audit.rules') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode '640' }
+      its(:md5sum) { should eq '0e926ccef2304bb653b4c8a60e8cea0a' }
+    end
   end
-  describe file('/etc/audit/audit.rules') do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    it { should be_mode '640' }
-    its(:md5sum) { should eq '0e926ccef2304bb653b4c8a60e8cea0a' }
+  
+  describe 'auditd::service class' do
+    describe service('auditd') do
+      it { should be_enabled }
+      it { should be_running }
+    end
   end
-end
 
-describe 'auditd::service class' do
-  describe service('auditd') do
-    it { should be_enabled }
-    it { should be_running }
-  end
 end
-
