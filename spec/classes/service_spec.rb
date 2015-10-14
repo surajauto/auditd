87c08e51f5984924e608c5c1217c0dd46dccadcd
Luther Barnum <luther.barnum@gmail.com>
2015-10-14 10:42:18 -0400

    Removed testing code for server temporarily

---
+++ spec/classes/service_spec.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env rspec
+require 'spec_helper'
+describe 'auditd::service', :type => :class do
+    #let(:service_manage) => { { 'true' } }
+    #let(:params) { { :service_ensure => 'file' ,:service_enable => 'true', :service_name => 'auditd' ,:service_hasstatus => 'true', :service_hasrestart => 'true', :service_manage => 'true'} }
+
+  context 'with defaults for all parameters' do
+#    it { should contain_class('auditd::service') }
+#    it do
+#      should contain_service('auditd').with(
+#        'ensure'       => 'running',
+#        'enable'       => 'true',
+#        'name'         => 'auditd',
+#        'hasstatus'    => 'true',
+#        'hasrestart'   => 'true',
+#    ) end
+  end
+end
