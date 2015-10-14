bee40b50bcdb298ba65a49cfd57ba3e40ff0af5d
Luther Barnum <luther.barnum@gmail.com>
2015-10-14 09:58:43 -0400

    Removed extra class check

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe 'auditd::config', :type => :class do
     let(:params) { { :auditd_file_ensure => 'file' ,:auditd_file_source => 'puppet:///modules/auditd/auditd.conf', :auditd_file_owner => 'root' ,:auditd_file_group => 'root', :auditd_file_mode => '0640', :audit_rules_file_ensure => 'file' ,:audit_rules_file_source => 'puppet:///modules/auditd/audit.rules', :audit_rules_file_owner => 'root' ,:audit_rules_file_group => 'root', :audit_rules_file_mode => '0640' } }
 
-    it { should contain_class('auditd::config') }
   context 'with defaults for all parameters' do
     it { should contain_class('auditd::config') }
     it do
