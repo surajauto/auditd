6de68a176bcbac4809449d577713f88cb705cf4c
Levi Jarick <github@ubeek.net>
2021-08-02 13:41:04 +0800

    Remove unused params spec file

--- spec/classes/params_spec.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env rspec
-require 'spec_helper'
-describe 'auditd::params' do
-  context 'with defaults for all parameters' do
-    it { is_expected.to contain_class('auditd::params') }
-  end
-end
